08f69c5d636cf79c111c9053e7abaf5245f99935
buibahoanvu <82458363+buibahoanvu@users.noreply.github.com>
2021-09-27 21:51:37 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,22 @@
+const CONFIG = {
+    introTitle: 'Xin Chào',
+    introDesc: `Rất vui khi gặp nàng`,
+    btnIntro: 'Bắt Đầu',
+    title: 'Bạn Làm Người Yêu Mình Nhé!',
+    desc: 'Đừng hòng chạy thoát',
+    btnYes: 'Đồng Ý luôn',
+    btnNo: 'CÚT',
+    question:'BẠN THÍCH MÌNH ĐIỂM NÀO',
+    btnReply: 'XÁC NHẬN',
+    reply: 'TẤT CẢ MỌI THỨ',
+    mess: 'CÁM ƠN Vì ĐÃ CHÁP NHẬN MÌNH',
+    messDesc: 'saranghae',
+    btnAccept: 'KHÔNG CÓ CHI',
+    messLink: 'https://www.facebook.com/profile.php?id=100014318509334', //link fb
+    bgLink: 'https://huypham9205.github.io/tang-crush/img/bg.webp', //link ảnh nền
+    iputBgLink: 'https://recmiennam.com/wp-content/uploads/2020/09/hinh-anh-cap-doi-lang-man-1.jpg', //link iputBgLink
+    lookMeLink: 'https://i.pinimg.com/236x/02/6e/51/026e512fd54018e1ad24875aca8ccb8d.jpg', //link ảnh lookMe
+    soundLink: 'https://huypham9205.github.io/tang-crush/sound/sound.mp3', //link nhac
+    duckLink: 'https://huypham9205.github.io/tang-crush/sound/duck.mp3', //link aam thanh 1
+    swishLink: 'https://huypham9205.github.io/tang-crush/sound/Swish1.mp3', //link swish
+}
